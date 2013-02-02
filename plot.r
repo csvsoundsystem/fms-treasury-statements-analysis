721a81e19d94712dfc729afc03e8a845ee2e1a7e
Thomas Levine <occurrence@thomaslevine.com>
2013-02-02 13:47:19 -0500

    aoeu

--- plot.r
+++ plot.r
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(scales)
 
-p.day <- ggplot(fms.day) + aes(color = item, x = date, y = today, group = item) +
+p.day <- ggplot(fms.item) + aes(color = item, x = date, y = today, group = item) +
     scale_x_date('Date') +
     scale_y_continuous('Amount', label = dollar) +
     geom_line()
@@ -24,3 +24,19 @@ p.rolling.joined <- ggplot(fms.rolling.joined.molten) + aes(x = value, y = perce
     scale_y_continuous('Mentions of "spending" per total words', label = percent) +
     labs(title = 'Discussion of spending has nothing to do with daily spending') +
     geom_point(alpha = 0.3)
+
+
+fms.item$day.of.month <- as.numeric(strftime(fms.item$date, format = '%d'))
+p.day.of.month <- ggplot(fms.item) +
+    aes(x = day.of.month, y = today, group = item, color = item) +
+    scale_x_continuous('Day of month') +
+    scale_y_continuous('Amount', label = dollar) +
+    labs(table = 'Spending is above or below $50,000 unless it\'s the middle or end of the month') +
+    geom_jitter()
+
+p.day.of.month.withdraw <- ggplot(subset(fms.item, item == 'Total Withdrawals (excluding transfers)')) +
+    aes(x = day.of.month, y = today, group = item, color = item) +
+    scale_x_continuous('Day of month') +
+    scale_y_continuous('Amount', label = dollar) +
+    labs(table = 'Spending is above or below $50,000 unless it\'s the middle or end of the month') +
+    geom_jitter()
