9acb751d761fe0cc08ce103b8ed16ace785dbe86
Thomas Levine <occurrence@thomaslevine.com>
2013-02-02 13:02:59 -0500

    title

--- plot.r
+++ plot.r
@@ -16,11 +16,11 @@ p.rolling <- ggplot(melt(fms.rolling, 'date', variable.name = 'Item', value.name
 p.joined <- ggplot(fms.joined.molten) + aes(x = value, y = percentage, color = variable) +
     scale_x_continuous('Cash flow', label = dollar) +
     scale_y_continuous('Mentions of "spending" per total words', label = percent) +
-    labs(table = 'Discussion of spending is higher when net change is near zero') +
+    labs(title = 'Discussion of spending is higher when net change is near zero') +
     geom_point(alpha = 0.3)
 
 p.rolling.joined <- ggplot(fms.rolling.joined.molten) + aes(x = value, y = percentage, color = variable) +
-    scale_x_continuous('Cash flow', label = dollar) +
+    scale_x_continuous('Cash flow (7-business-day rolling mean)', label = dollar) +
     scale_y_continuous('Mentions of "spending" per total words', label = percent) +
-    labs(table = 'Discussion of spending is higher when net change is near zero') +
+    labs(title = 'Discussion of spending has nothing to do with daily spending') +
     geom_point(alpha = 0.3)
