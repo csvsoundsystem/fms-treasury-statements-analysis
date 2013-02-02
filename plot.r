25d01e7fef3c323328ad4764836f3fd861b99183
Thomas Levine <occurrence@thomaslevine.com>
2013-02-02 01:40:39 -0500

    plot

--- plot.r
+++ plot.r
@@ -9,5 +9,6 @@ p.day <- ggplot(fms.day) + aes(color = item, x = date, y = today, group = item) 
 p.rolling <- ggplot(melt(fms.rolling, 'date', variable.name = 'Item', value.name = 'Amount')) +
     aes(color = Item, x = date, y = Amount, group = Item) +
     scale_x_date('Date') +
-    scale_y_continuous('Amount', label = dollar) +
+    scale_y_continuous('Amount (7-business-day moving average)', label = dollar) +
+    labs(title = 'Daily federal cash flow') +
     geom_line()
