d9217ad7c307f0847874e6917de10a4a6f48fc22
Thomas Levine <occurrence@thomaslevine.com>
2013-02-02 11:57:38 -0500

    insert

--- capitolwords-insert.py
+++ capitolwords-insert.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python2
 
 import os, sys
+import json
 import dumptruck
 
 dt = dumptruck.DumpTruck(dbname = '/tmp/capitolwords.db')
 
-f = open(sys.argv([1]))
+phrase = sys.argv[1]
+filename = os.path.join('capitolwords', phrase + '.json')
+print('Loading ' + filename)
+f = open(filename)
 d = json.load(f)
 f.close()
 
-dt.insert(d['results'])
+dt.create_table(d['results'][0], phrase)
+dt.create_index(['day'], phrase, unique = True)
+dt.insert(d['results'], phrase)
