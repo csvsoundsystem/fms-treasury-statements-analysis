219688949da1250606b56711c9aed7a3692afb57
Brian Abelson <brianabelson@gmail.com>
2013-02-02 10:57:02 -0500

    added raw fixie files

---
+++ brian.r
@@ -0,0 +1,42 @@
+library(reshape2)
+library(stringr)
+setwd("/Users/brian/Dropbox/code/fms-treasury-statements-analysis")
+strip <- function(string) {
+    # Strip leading and trailing spaces
+    str_replace(str_replace(string, '^ *', ''), ':? *$', '')
+}
+
+number <- function(string){
+    # Remove the leading dollar sign, remove commas, and convert to numeric.
+    as.numeric(str_replace_all(str_replace(strip(string), '^[$]?(?:[0-9]/)?', ''), ',', ''))
+}
+
+fms <- read.fwf('summary.fixie',
+    c(9, 50, 13, 1, 13, 1, 13), skip = 1,
+    col.names = c('file', 'item', 'today', 'space1', 'mtd', 'space2', 'ytd')
+)[-c(4,6)]
+
+# Remove na
+fms <- na.omit(fms)
+
+# Strip spaces
+fms <- data.frame(lapply(fms, strip))
+
+# Make numbers
+fms[c('today', 'mtd', 'ytd')] <- data.frame(lapply(fms[c('today', 'mtd', 'ytd')], number))
+
+# Make dates
+fms$date <- as.Date(strptime(fms$file, format = '%Y%m%d'))
+fms$file <- NULL
+head(fms)
+# Different slices
+fms.item <- fms
+fms.day <- dcast(fms, date ~ item, value.var = 'today')
+names(fms.day) <- c("date", "chance", "deposits", "withdrawals")
+
+
+# Rolling mean
+fms.rolling <- data.frame(lapply(fms.day[-1], rollmean, k = 7))
+fms.rolling$date <- fms.day$date[4:(nrow(fms.day) - 3)]
+
+
