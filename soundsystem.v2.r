7b6b656dbc6dd71afc90e6e6f412249c0b6780dc
Thomas Levine <occurrence@thomaslevine.com>
2013-02-03 02:16:35 -0500

    load

--- soundsystem.v2.r
+++ soundsystem.v2.r
@@ -1,26 +1,30 @@
 library(plyr)
+library(aplpack)
 
 # Load the data
-table2.raw <- read.csv('table2-std.csv')
+if (!'table2.raw' %in% ls()) {
+    table2.raw <- read.csv('table2-std.csv')
+
+    # Fix types
+    table2.raw$date <- as.Date(table2.raw$date)
+    table2.raw$type <- factor(table2.raw$type)
+    table2.raw$item <- factor(table2.raw$item)
+    table2.raw$today <- as.numeric(table2.raw$today)
+}
 table2 <- table2.raw[!table2.raw$is_total,c('date', 'type', 'item', 'today')]
 
-# Fix types
-table2$date <- as.Date(table2$date)
-table2$type <- factor(table2$type)
-table2$item <- factor(table2$item)
-table2$today <- as.numeric(table2$today)
 
 # Make faster
-table2 <- table2[1:1000,]
+table2 <- table2[table2$date > as.Date('2013-01-02'),]
 
 # Select only the items that are present on all days.
 n.days <- length(unique(table2$date))
-table2 <- ddply(table2, c('type', 'item'), function(df) {
+table2.pca <- ddply(table2, c('type', 'item'), function(df) {
     if (nrow(df) == n.days) {
         df
     }
 })
 
 # Run PCA
-
-items <- dcast(table2, date ~ type + item, value.var = 'today')[-1]
+items <- dcast(table2.pca, date ~ type + item, value.var = 'today')[-1]
+pca <- princomp(items, cor = T)
