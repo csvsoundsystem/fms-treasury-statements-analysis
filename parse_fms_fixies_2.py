b5ac336efcbb34101737c0d403170f405e62bfe2
Burton DeWilde <burtondewilde@gmail.com>
2013-02-05 23:43:21 -0500

    converting over to plain ol python file, now with more defs

---
+++ parse_fms_fixies_2.py
@@ -0,0 +1,207 @@
+
+import collections
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pprint import pprint
+import re
+import StringIO
+
+DIR_NAME = '/Users/bjdewilde/Desktop/hackathon/data/fixies/'
+
+def get_date_and_day(f_name):
+	raw_date = re.search(r'(\d+).txt', f_name).group(1)
+	date = datetime.date(2000+int(raw_date[0:2]), int(raw_date[2:4]), int(raw_date[4:6]))
+	day = datetime.datetime.strftime(date, '%A')
+	return date, day
+
+def get_table_name(line):
+	try:
+		table_line = re.search(r'\s+TABLE.*', line).group()
+		table = table_line.strip()
+	except AttributeError:
+		table = None
+	return table
+
+def normalize_page_text(page):
+	# split on line breaks
+	lines = re.split(r'\r\n', page)
+	# get rid of pipe delimiters and divider lines
+	lines = [re.sub(r'^ \|', '       ', line) for line in lines]
+	lines = [re.sub(r'\|', '', line) for line in lines]
+	lines = [re.sub(r'\s?_{5,}', '', line) for line in lines]
+	# get rid of dollar signs and thousand commas
+	lines = [re.sub(r'\$', '', line) for line in lines]
+	lines = [re.sub(r'(\d),(\d)', r'\1\2', line) for line in lines]
+	# normalize non-leading white space
+	lines = [line[:6] + re.sub(r'\s{2,}', ' ', line[6:]) for line in lines]
+	lines = [line.rstrip() for line in lines]
+	# get rid of blank lines
+	lines = [line for line in lines if line!='' and line!=' ']
+	return lines
+
+def get_footnote(line):
+	try:
+		footnote_match = re.search(r'^\s?(\d)\/(.*)', line)
+		footnote = [footnote_match.group(1), footnote_match.group(2)]
+	except AttributeError:
+		footnote = None
+	return footnote
+
+def parse_page(f):
+	f_name = '13013100.txt'
+	f = open(DIR_NAME + f_name, 'r').read()
+
+	raw_pages = re.split(r'\d.*DAILY TREASURY STATEMENT.*PAGE:\s+\d\s{2}', f)
+	pages = []
+	for raw_page in raw_pages:
+		page = normalize_page_text(raw_page)
+		pages.append(page)
+
+	i = 4
+
+	# metadata
+	date = get_date_and_day(f_name)[0]
+	day = get_date_and_day(f_name)[1]
+	print date, day
+	print table_name, "\n"
+
+	for line in pages[i]:
+		table_name = get_table_name(line)
+		if table_name: break
+
+	# table data
+	indent = 0
+	table = []
+	footnotes = {}
+	surtype_index = -1; subtype_index = -1; used_index = -1
+	subtype_indent = -1
+	type_ = None; subtype = None
+	for line in pages[i]:
+		#print line
+		row = {}
+		
+		# fill in metadata
+		row['table'] = table_name
+		row['date'] = date
+		row['day'] = day
+		
+		index = pages[i].index(line)
+		if index == used_index : continue
+		indent = len(re.search(r'^\s*', line).group())
+		
+		# skip table header rows
+		if re.match(r'\s{7,}', line): continue
+		if get_table_name(line): continue
+		
+		# save footnotes for later assignment to their rows
+		if get_footnote(line):
+			footnote = get_footnote(line)
+			footnotes[footnote[0]] = footnote[1]
+			continue
+		# note rows with footnotes for later assignment
+		if re.search(r'\d\/ ', line):
+			row['footnote'] = re.search(r'(\d)\/ ', line).group(1)
+		
+		# separate digits and words
+		digits = re.findall(r'(\d+)', line)
+		words = re.findall(r'[^\W\d]+:?', line)
+		text = ' '.join(words)
+		
+		# get type row
+		if len(digits) == 0 and text.endswith(':') and indent == 1:
+			type_ = text[:-1]
+			type_indent = indent
+			type_index = index
+			continue
+		if index == type_index + 1: pass
+		elif indent <= type_indent:
+			type_ = None
+		row['type'] = type_
+			
+		# get subtype row
+		if len(digits) == 0 and text.endswith(':'):
+			subtype = text[:-1]
+			subtype_indent = indent
+			subtype_index = index
+			continue
+		if index == subtype_index + 1: pass
+		elif indent <= subtype_indent:
+			subtype = None
+		row['subtype'] = subtype
+		
+		# get and merge two-line rows
+		if len(digits) == 0 and not text.endswith(':'):
+			try:
+				next_line = pages[i][index + 1]
+				next_digits = re.findall(r'(\d+)', next_line)
+				next_words = re.findall(r'[^\W\d]+:?', next_line)
+				if len(next_digits) != 0:
+					text = text + ' ' + ' '.join(next_words)
+					digits = next_digits
+					used_index = index + 1
+			except IndexError: pass
+		
+		# skip table annotations that aren't footnotes
+		# this is approximate at best
+		if len(digits) == 0: continue
+			
+		row['is_total'] = int('total' in text.lower())
+		
+		if i == 1:
+			row['account'] = text
+			row['close_today'] = digits[-4]
+			row['open_today'] = digits[-3]
+			row['open_mo'] = digits[-2]
+			row['open_fy'] = digits[-1]
+		elif i == 7:
+			row['classification'] = text
+			row['today'] = digits[-3]
+			row['mtd'] = digits[-2]
+			row['fytd'] = digits[-1]
+		elif i in [2, 3]:
+			row['item'] = text
+			row['today'] = digits[-3]
+			row['mtd'] = digits[-2]
+			row['fytd'] = digits[-1]
+			# tweak column names
+			row['account'] = row['type']
+			if i == 2:
+				row['type'] = 'deposit'
+			elif i == 3:
+				row['type'] = 'withdrawal'
+		elif i in [4, 5]:
+			row['item'] = text
+			try:
+				row['today'] = digits[-3]
+				row['mtd'] = digits[-2]
+				row['fytd'] = digits[-1]
+			except:
+				print "WARNING:", line
+			
+		table.append(row)
+
+	for row in table:
+		try:
+			row['footnote'] = footnotes[row['footnote']]
+		except KeyError: pass
+		if row['item'].lower().strip() == 'total issues':
+			surtype_index = table.index(row)
+			row['surtype'] = 'issue'
+
+	if surtype_index != -1:
+		for row in table[:surtype_index]:
+			row['surtype'] = 'issue'
+		for row in table[surtype_index + 1:]:
+			row['surtype'] = 'redemption'
+
+
+	df = pd.DataFrame(table)
+
+	if i == 1:
+		df = df.reindex(columns=['table', 'date', 'day', 'account', 'type', 'item', 'is_total', 'close_today', 'open_today', 'mtd', 'fytd', 'footnote'])
+	if i in [2,3]:
+		df = df.reindex(columns=['table', 'date', 'day', 'account', 'type', 'subtype', 'item', 'is_total', 'today', 'mtd', 'fytd', 'footnote'])
+	df
+
